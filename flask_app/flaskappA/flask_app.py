f445af99d0f5248c9baeb9948fd4fc49cf88c3e2
NetAcad DEVASC <changeme@example.com>
2026-01-29 06:28:41 +0000

    update

--- flask_app/flaskappA/flask_app.py
+++ flask_app/flaskappA/flask_app.py
@@ -11,4 +11,5 @@ def main():
     return render_template("index.html" , datetime_now = datetime.datetime.now())
 
 if __name__ == "__main__":
-    microweb_app.run(host="0.0.0.0", port=5555)
+    # New version: Disable threading to prevent the crash
+    microweb_app.run(host="0.0.0.0", port=5555, threaded=False)
